0ab295ebfa13e53e96828901b3b8a5eb52ebbd5e
JohnConnellCS <johnconnellcs@gmail.com>
2024-08-03 21:31:13 -0700

    Debugged Firestore issue and began implementation of quantity feature

--- firebase.js
+++ firebase.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCM3C0iqRoXLMcV0kIr3vpez8fN3tra388",
-  authDomain: "inventory-management-app-bbcac.firebaseapp.com",
-  projectId: "inventory-management-app-bbcac",
-  storageBucket: "inventory-management-app-bbcac.appspot.com",
-  messagingSenderId: "823295447423",
-  appId: "1:823295447423:web:860f76d7e5dc6793798a4a",
-  measurementId: "G-HKFN0SCLGP"
+  apiKey: "AIzaSyAStyPLRuiPOMi3mz_sNY1aylIVRidnLcY",
+  authDomain: "inven-track-99195.firebaseapp.com",
+  projectId: "inven-track-99195",
+  storageBucket: "inven-track-99195.appspot.com",
+  messagingSenderId: "549358519187",
+  appId: "1:549358519187:web:0d590ce65ea93bf8abde66"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
 export { firestore };
